2d9ad4bf6a6a0845d3ced2904bd958d56eeb19e4
andymaheshw <andymaheshw@gmail.com>
2015-07-27 21:25:22 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -6,69 +6,72 @@ function loadData() {
     var $nytHeaderElem = $('#nytimes-header');
     var $nytElem = $('#nytimes-articles');
     var $greeting = $('#greeting');
-
+     console.log($nytElem);
     // clear out old data before new request
     $wikiElem.text("");
     $nytElem.text("");
 
-    var streetStr = $('#street').val();
-    var cityStr = $('#city').val();
-    var address = streetStr + ', ' + cityStr;
-
-    $greeting.text('So, you want to live at ' + address + '?');
-
     // load streetview
-    var streetviewUrl = 'http://maps.googleapis.com/maps/api/streetview?size=600x400&location=' + address + '';
-    $body.append('<img class="bgimg" src="' + streetviewUrl + '">');
+    streetStr = $('#street').val();
+    cityStr = $('#city').val();
+    addressStr = streetStr + ', '+cityStr;
+    console.log(addressStr);
 
+    $greeting.text("You want to live at: "+ streetStr + " and "+ cityStr);
+    var streetviewURL='http://maps.googleapis.com/maps/api/streetview?size=600x400&location='+addressStr;
+    //var streetviewURL = 'http://maps.googleapis.com/maps/api/streetview?size=600x400&location=1324%20Locust%20St,%20Philadelphia';
+    $body.append("<img class='bgimg' src="+streetviewURL+ '">');
 
-    // load nytimes
-    var nytimesUrl = 'http://api.nytimes.com/svc/search/v2/articlesearch.json?q=' + cityStr + '&sort=newest&api-key=3abc9a3d23e60b38c21b4ab9b0a91c07:17:69911633'
-    $.getJSON(nytimesUrl, function(data){
-
-        $nytHeaderElem.text('New York Times Articles About ' + cityStr);
+    // YOUR CODE GOES HERE!
+    var nytimesUrl = "http://api.nytimes.com/svc/search/v2/articlesearch.json?q="+cityStr+'&sort=newest&api-key=895046fa5c4193a15351189f360480d5:8:56255201'; 
+    $.getJSON(nytimesUrl , function (data) {
+        
+        $nytHeaderElem.text("NYT articles about "+ cityStr);
 
         articles = data.response.docs;
-        for (var i = 0; i < articles.length; i++) {
+        console.log(articles[0]);
+        for (var i = 0; i < articles.length; i++)
+        {
             var article = articles[i];
-            $nytElem.append('<li class="article">'+
-                '<a href="'+article.web_url+'">'+article.headline.main+'</a>'+
-                '<p>' + article.snippet + '</p>'+
-            '</li>');
-        };
-
-    }).error(function(e){
-        $nytHeaderElem.text('New York Times Articles Could Not Be Loaded');
-    });
-
-
-
-    // load wikipedia data
-    var wikiUrl = 'http://en.wikipedia.org/w/api.php?action=opensearch&search=' + cityStr + '&format=json&callback=wikiCallback';
-    var wikiRequestTimeout = setTimeout(function(){
-        $wikiElem.text("failed to get wikipedia resources");
-    }, 8000);
-
-    $.ajax({
-        url: wikiUrl,
-        dataType: "jsonp",
-        jsonp: "callback",
-        success: function( response ) {
-            var articleList = response[1];
-
-            for (var i = 0; i < articleList.length; i++) {
-                articleStr = articleList[i];
-                var url = 'http://en.wikipedia.org/wiki/' + articleStr;
-                $wikiElem.append('<li><a href="' + url + '">' + articleStr + '</a></li>');
-            };
-
-            clearTimeout(wikiRequestTimeout);
+            $nytElem.append('<li class="article">'
+                +'<a href="'+article.web_url+'">'+article.headline.main+
+                '</a>'+ '<p>'+article.snippet + '</p>' + '</li>');
         }
+       
+
+    }).error(function(e)
+    { 
+    $nytElem.append('<p> Not found </p>');
+
     });
+
+    var wikiURL = 'http://en.wikipedia.org/w/api.php?action=opensearch'+
+    '&search='+ cityStr + '&format=json&callback=wikiCallback';
+
+    $.ajax( {url: wikiURL, 
+        dataType: "jsonp",
+    success: function(response) {
+        var articlesList = response[1];
+        for (var i = 0; i < articlesList.length; i++)
+        {
+            var articleStr = articlesList[i];
+            var url = 'http://en.wikipedia.org/wiki/'+articleStr;
+            $wikiElem.append('<li>'
+                +'<a href="'+url+'">'+articleStr+
+                '</a>'+ '</li>');
+        }
+    }
+     });
 
     return false;
 };
 
 $('#form-container').submit(loadData);
 
-// loadData();
+function test() {
+
+    console.log("hello2");
+    return false;
+}
+
+ //loadData();
